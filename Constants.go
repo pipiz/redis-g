e22bde2ba8190a4e847921ebcbfc236d18fe369d
pipiz <zkx6648@gmail.com>
2019-02-01 17:34:08 +0800

    HashZipList的解析

--- Constants.go
+++ Constants.go
@@ -11,5 +11,12 @@ const Colon = ':'
 const Aux = 0xFA
 const DbSelector = 0xFE
 const DbResize = 0xFB
-const _string = 0
+const String = 0
 const Eof = 0xFF
+const HashZipList = 13
+
+const ZipInt8Bit = 0xFE  /* 11111110*/
+const ZipInt16Bit = 0xC0 /* 11000000*/
+const ZipInt24Bit = 0xF0 /* 11110000*/
+const ZipInt32Bit = 0xD0 /* 11010000*/
+const ZipInt64Bit = 0xE0 /* 11100000*/
--- RdbParser.go
+++ RdbParser.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	bytes2 "bytes"
 	"encoding/binary"
 	"fmt"
 	"strconv"
 )
 
 type Length struct {
-	Len     int
+	val     int
 	Special bool
 }
 
@@ -31,14 +32,40 @@ func parseRdb(size int) string {
 			parseAUX()
 		case DbSelector:
 			length := readLength()
-			fmt.Println("db:", length.Len)
+			fmt.Println("db:", length.val)
 		case DbResize:
 			len1 := readLength()
 			len2 := readLength()
-			fmt.Println("db total keys:", len1.Len)
-			fmt.Println("db expired keys:", len2.Len)
-		case _string:
-			fmt.Printf("key: %s, value: %s\n", readString(), readString())
+			fmt.Println("db total keys:", len1.val)
+			fmt.Println("db expired keys:", len2.val)
+		case String:
+			fmt.Printf("%s: %s\n", readString(), readString())
+		case HashZipList:
+			name := readString()
+			fmt.Printf("%s: {\n", name)
+
+			length := readLength()
+			bytes := make([]byte, length.val)
+			reader.Read(bytes)
+			byteReader := bytes2.NewReader(bytes)
+
+			arr := make([]byte, 4)
+			byteReader.Read(arr)
+
+			byteReader.Read(arr)
+
+			arr = arr[:2]
+			byteReader.Read(arr)
+			zlLen := binary.LittleEndian.Uint16(arr)
+
+			for zlLen > 0 {
+				field := readZipListEntry(byteReader)
+				zlLen--
+				value := readZipListEntry(byteReader)
+				zlLen--
+				fmt.Printf("\t%s: %s\n", string(field), string(value))
+			}
+			fmt.Println("}")
 		case Eof:
 			if version >= 5 {
 				checksum := readInteger(8, true)
@@ -54,10 +81,74 @@ func parseAUX() {
 	fmt.Printf("%s: %s\n", readString(), readString())
 }
 
+func readZipListEntry(byteReader *bytes2.Reader) []byte {
+	byt, _ := byteReader.ReadByte()
+	prevLen := uint32(byt)
+	if prevLen >= 254 {
+		bytes := make([]byte, 4)
+		byteReader.Read(bytes)
+		prevLen = binary.LittleEndian.Uint32(bytes)
+	}
+	specialFlag, _ := byteReader.ReadByte()
+	switch specialFlag >> 6 {
+	case 0:
+		length := specialFlag & 0x3F // 余下6位就是长度
+		bytes := make([]byte, length)
+		byteReader.Read(bytes)
+		return bytes
+	case 1:
+		b, _ := byteReader.ReadByte() // 再读一个字节, 合并14位就是长度
+		length := ((int16(specialFlag) & 0x3F) << 8) | int16(b)
+		bytes := make([]byte, length)
+		byteReader.Read(bytes)
+		return bytes
+	case 2:
+		bytes := make([]byte, 4) // 再读四个字节，就是长度
+		byteReader.Read(bytes)
+		length := binary.BigEndian.Uint32(bytes)
+		bytes = make([]byte, length)
+		byteReader.Read(bytes)
+		return bytes
+	default:
+		break
+	}
+	switch specialFlag {
+	case ZipInt8Bit:
+		b, _ := byteReader.ReadByte()
+		return []byte(strconv.Itoa(int(b)))
+	case ZipInt16Bit: // little endian
+		bytes := make([]byte, 2)
+		byteReader.Read(bytes)
+		u := binary.LittleEndian.Uint16(bytes)
+		return []byte(strconv.Itoa(int(u)))
+	case ZipInt24Bit: // little endian
+		bytes := make([]byte, 4)
+		bytes[0], _ = byteReader.ReadByte()
+		bytes[1], _ = byteReader.ReadByte()
+		bytes[2], _ = byteReader.ReadByte()
+		u := binary.LittleEndian.Uint32(bytes)
+		return []byte(strconv.Itoa(int(u)))
+	case ZipInt32Bit: // little endian
+		bytes := make([]byte, 4)
+		byteReader.Read(bytes)
+		u := binary.LittleEndian.Uint32(bytes)
+		return []byte(strconv.Itoa(int(u)))
+	case ZipInt64Bit: // little endian
+		bytes := make([]byte, 8)
+		byteReader.Read(bytes)
+		u := binary.LittleEndian.Uint64(bytes)
+		return []byte(strconv.Itoa(int(u)))
+	default:
+		result := specialFlag - 0xF1
+		return []byte(strconv.Itoa(int(result)))
+	}
+	return nil
+}
+
 func readString() string {
 	length := readLength()
 	if length.Special {
-		switch length.Len {
+		switch length.val {
 		case 0:
 			b, _ := reader.ReadByte()
 			return strconv.Itoa(int(b))
@@ -71,7 +162,7 @@ func readString() string {
 			// TODO LZF压缩的字符串
 		}
 	}
-	bytes := make([]byte, length.Len)
+	bytes := make([]byte, length.val)
 	reader.Read(bytes)
 	return string(bytes)
 }
@@ -82,7 +173,7 @@ func readLength() Length {
 
 	b, _ := reader.ReadByte()
 
-	switch b & 0xC0 >> 6 { // 取高二位
+	switch (b & 0xC0) >> 6 { // 取高二位
 	case 0: // 00, 余下6位表示长度
 		length = int(b & 0x3F)
 	case 1: // 01, 再读一字节, 组合14位表示长度
